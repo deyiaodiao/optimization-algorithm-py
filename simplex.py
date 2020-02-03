0b48cfcb0bd2984052eb9d9ec5946b3944cfa17b
deyiaodiao <deyiaodiao@gmail.com>
2020-02-03 16:54:19 +0800

    Add files via upload

--- simplex.py
+++ simplex.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-## max 5x1+6x2+7x4
-## x1+x2+2x3<=1
-## x1+2x2+x3<=2
+## max 50x1+100x2
+## x1+x2<=300
+## 2x1+x2<=400
+## x2<=250
 
 M=10000
-####确定数据
+
 ##系数矩阵-标准化后
 A=[[1,1,1,0,0],[2,1,0,1,0],[0,1,0,0,1]]
 A=np.mat(A)
@@ -17,14 +18,13 @@ c=[[50],[100],[0],[0],[0]]
 c=np.mat(c)
 
 m=A.shape[0]
+column=A.shape[1]
 
 xb=[2,3,4]
 xn=[0,1]
 
 xb.sort()
 xn.sort()
-max_a=max(xb)
-print(max_a)
 
 def calc_sigma(_BI):
     ctn=c[xn].T
@@ -45,57 +45,68 @@ def calc_theta(_BI,_xin):
     theta=np.array(theta)
     return theta
 
-stop=0
-while(stop==0):
-    print(stop,"stop")
-    print(xb,"xb",xn,"xn")
-    B=A[:,xb]
-    print(B,"B")
-    BI=B.I
-    sigma=calc_sigma(BI)
+def calc_obj(_xb,_xn):
+    sol=[]
+    for i in range(0,column):
+        sol.append(0)
+    _B=A[:,_xb]
+    _BI=_B.I
+    _bi=_BI.dot(b)
+    index=0
+    for i in _xb:
+        sol[i]=float(_bi[index])
+        index=index+1
+    sol=np.mat(sol)
+    obj=sol.dot(c)
 
-    print(sigma,"检验数")
+    return obj
+
+
+if __name__=='__main__':
+    stop=0
+    while(stop==0):
+        print(xb,"xb",xn,"xn")
+        B=A[:,xb]
+        print(B,"B")
+        BI=B.I
+        sigma=calc_sigma(BI)
+
+        print(sigma,"检验数")
     
-    print(BI.dot(b),"b")
+        print(BI.dot(b),"b")
 
-    #存在使目标函数优化的非基变量
-    if((sigma>0).any()==True):
-        stop=0
-    else:
-        stop=1
-        continue
+        #存在使目标函数优化的非基变量
+        if((sigma>0).any()==True):
+            stop=0
+        else:#最优
+            stop=1
+            continue
         
-    #入基变量
-    xin=xn[np.argmax(sigma)]
+        #入基变量
+        xin=xn[np.argmax(sigma)]
 
-    #计算出基变量的theta值
-    theta=calc_theta(BI,xin)
-    print(theta,"theta")
-    if((theta>0).any()==True):
-        stop=0
-    else:
-        stop=1
-        continue
-    #出基变量
-    xout=xb[np.argmin(theta)]
+        #计算出基变量的theta值
+        theta=calc_theta(BI,xin)
+        print(theta,"theta")
+        if((theta>0).any()==True):
+            stop=0
+        else:#无界
+            stop=2
+            continue
+        #出基变量
+        xout=xb[np.argmin(theta)]
 
-    #更新基可行解
-    xb.remove(xout)
-    xb.append(xin)
-    xn.remove(xin)
-    xn.append(xout)
-    xb.sort()
-    xn.sort()
-    print(xin,"xin",xout,"xout")
-    print("------------------------------------")
+        #更新基可行解
+        xb.remove(xout)
+        xb.append(xin)
+        xn.remove(xin)
+        xn.append(xout)
+        xb.sort()
+        xn.sort()
+        print(xin,"xin",xout,"xout")
+        print("------------------------------------")
+    print(calc_obj(xb,xn),"obj")
 
-
-
-
-##C_A = c[[0,2]]    #先取出想要的行数据
-##C_A = C_A[:,[2,3]] #再取出要求的列数据
-
-
-
+    
 
 
